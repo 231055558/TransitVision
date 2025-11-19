25aa5590537444522993e0766ae35085c1547ebb
231055558 <231055558@qq.com>
2025-11-19 16:32:16 +0800

    fix: 更新logic模块导出，使用类式API

--- transit_vision/logic/__init__.py
+++ transit_vision/logic/__init__.py
@@ -1,10 +1,12 @@
 from .boarding_counter import filter_boarding_passengers
-from .alighting_counter import filter_alighting_passengers
+from .alighting_counter import AlightingCounter
+from .alighting_counter_v1 import AlightingCounterV1
 from .door_preprocessor import preprocess_front_door, preprocess_rear_door
 
 __all__ = [
     'filter_boarding_passengers',
-    'filter_alighting_passengers',
+    'AlightingCounter',
+    'AlightingCounterV1',
     'preprocess_front_door',
     'preprocess_rear_door'
 ]
